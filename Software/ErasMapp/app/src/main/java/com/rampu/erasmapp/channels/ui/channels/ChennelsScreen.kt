1ecdfedeeb27649da0e23dfebff51546f0ecd8cb
lkanjir23 <lkanjir23@student.foi.hr>
2025-12-24 19:55:47 +0100

    Connected the existing channel features with the new channels screen UI (#40)

--- Software/ErasMapp/app/src/main/java/com/rampu/erasmapp/channels/ui/channels/ChennelsScreen.kt
+++ Software/ErasMapp/app/src/main/java/com/rampu/erasmapp/channels/ui/channels/ChennelsScreen.kt
@@ -2,7 +2,6 @@
 
 package com.rampu.erasmapp.channels.ui.channels
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,21 +17,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rampu.erasmapp.channels.domian.Channel
-import com.rampu.erasmapp.common.ui.StandardPreview
 import com.rampu.erasmapp.common.ui.components.LabeledInputField
 import com.rampu.erasmapp.common.ui.components.LoadingIndicator
 import com.rampu.erasmapp.ui.theme.ErasMappTheme
@@ -44,139 +40,110 @@ fun ChannelsScreen(
     onEvent: (event: ChannelEvent) -> Unit,
     state: ChannelUiState
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-
-        ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Channels",
-                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Medium)
-            )
-            IconButton(
-                onClick = {}
+    when {
+        state.isLoading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Icon(
-                    Icons.Filled.Add,
-                    contentDescription = "Add channel"
-                )
+                LoadingIndicator()
             }
         }
-        Spacer(Modifier.height(10.dp))
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(20.dp)
-        ) {
-            items(state.channels, key = { it.id }) { channel ->
-                ChannelItem(
-                    channel,
-                    onClick = {},
-                    Modifier.clickable { onChannelSelected(channel.id, channel.title) })
+
+        state.showCreateDialog -> {
+            AlertDialog(
+                onDismissRequest = {
+                    onEvent(ChannelEvent.ShowCreateDialog(false))
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            onEvent(ChannelEvent.CreateChannel)
+                        }
+                    ) {
+                        Text("Save")
+                    }
+                },
+                dismissButton = {
+                    Button(
+                        onClick = {
+                            onEvent(ChannelEvent.ShowCreateDialog(false))
+                        }
+                    ) {
+                        Text("Cancel")
+                    }
+                },
+                text = {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(15.dp)
+                    ) {
+                        LabeledInputField(
+                            value = state.newTitle,
+                            onValueChange = {
+                                onEvent(ChannelEvent.TitleChanged(it))
+                            },
+                            label = "Title"
+                        )
+                        LabeledInputField(
+                            value = state.newTopic,
+                            onValueChange = {
+                                onEvent(ChannelEvent.TopicChanged(it))
+                            },
+                            label = "Topic"
+                        )
+                        LabeledInputField(
+                            value = state.newDescription,
+                            onValueChange = {
+                                onEvent(ChannelEvent.DescriptionChanged(it))
+                            },
+                            label = "Description"
+                        )
+                    }
+                }
+            )
+        }
+
+        else -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+
+                ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Channels",
+                        style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Medium)
+                    )
+                    IconButton(
+                        onClick = { onEvent(ChannelEvent.ShowCreateDialog(true)) }
+                    ) {
+                        Icon(
+                            Icons.Filled.Add,
+                            contentDescription = "Add channel"
+                        )
+                    }
+                }
+                Spacer(Modifier.height(10.dp))
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    items(state.channels, key = { it.id }) { channel ->
+                        ChannelItem(
+                            channel,
+                            onClick = { onChannelSelected(channel.id, channel.title) })
+                    }
+                }
             }
         }
     }
-
-
-//    when {
-//        state.isLoading -> {
-//            Box(
-//                modifier = Modifier.fillMaxSize(),
-//                contentAlignment = Alignment.Center
-//            ) {
-//                LoadingIndicator()
-//            }
-//        }
-//
-//        state.showCreateDialog -> {
-//            AlertDialog(
-//                onDismissRequest = {
-//                    onEvent(ChannelEvent.ShowCreateDialog(false))
-//                },
-//                confirmButton = {
-//                    Button(
-//                        onClick = {
-//                            onEvent(ChannelEvent.CreateChannel)
-//                        }
-//                    ) {
-//                        Text("Save")
-//                    }
-//                },
-//                dismissButton = {
-//                    Button(
-//                        onClick = {
-//                            onEvent(ChannelEvent.ShowCreateDialog(false))
-//                        }
-//                    ) {
-//                        Text("Cancel")
-//                    }
-//                },
-//                text = {
-//                    Column(
-//                        verticalArrangement = Arrangement.spacedBy(15.dp)
-//                    ) {
-//                        LabeledInputField(
-//                            value = state.newTitle,
-//                            onValueChange = {
-//                                onEvent(ChannelEvent.TitleChanged(it))
-//                            },
-//                            label = "Title"
-//                        )
-//                        LabeledInputField(
-//                            value = state.newTopic,
-//                            onValueChange = {
-//                                onEvent(ChannelEvent.TopicChanged(it))
-//                            },
-//                            label = "Topic"
-//                        )
-//                        LabeledInputField(
-//                            value = state.newDescription,
-//                            onValueChange = {
-//                                onEvent(ChannelEvent.DescriptionChanged(it))
-//                            },
-//                            label = "Description"
-//                        )
-//                    }
-//                }
-//            )
-//        }
-//
-//        else -> {
-//            Column(modifier = Modifier.fillMaxSize()) {
-//                LazyColumn(
-//                    modifier = Modifier.weight(1f),
-//                    verticalArrangement = Arrangement.spacedBy(15.dp)
-//                ) {
-//                    items(state.channels, key = { it.id }) { channel ->
-//                        Card(
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .clickable{
-//                                    onChannelSelected(channel.id, channel.title)
-//                                }
-//                        ) {
-//                            Text(text = channel.title)
-//                        }
-//                    }
-//
-//                }
-//                Button(
-//                    onClick = {
-//                        onEvent(ChannelEvent.ShowCreateDialog(true))
-//                    }
-//                ) {
-//                    Text("Add channel")
-//                }
-//            }
-//        }
-//    }
 }
 
 @Preview(showSystemUi = false, showBackground = true)
@@ -186,7 +153,7 @@ fun ChannelScreenPreview() {
         ChannelsScreen(
             onBack = {},
             onEvent = {},
-            onChannelSelected = { channelId, channelTitle -> },
+            onChannelSelected = { _, _ -> },
             state = ChannelUiState(
                 isLoading = false,
                 showCreateDialog = false,
